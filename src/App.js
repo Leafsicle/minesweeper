29c6f6d5b26a233a62b5c0b310591523ac0bd1de
matt <cookem529@gmail.com>
2018-09-28 11:14:01 -0400

    so far so good. keep up the componnent shift to the board game

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
- import './App.css';
-import Board from './Board'
+import './App.css';
+// import Board from './Board'
 import axios from "axios";
 
 class App extends Component {
@@ -10,14 +10,14 @@ class App extends Component {
     this.state = {
       id:1,
       board: [
-        [" "," "," "," "," "," "," "," "],
-        [" "," "," "," "," "," "," "," "],
-        [" "," "," "," "," "," "," "," "],
-        [" "," "," "," "," "," "," "," "],
-        [" "," "," "," "," "," "," "," "],
-        [" "," "," "," "," "," "," "," "],
-        [" "," "," "," "," "," "," "," "],
-        [" "," "," "," "," "," "," "," "]
+        ["","","","","","","",""],
+        ["","","","","","","",""],
+        ["","","","","","","",""],
+        ["","","","","","","",""],
+        ["","","","","","","",""],
+        ["","","","","","","",""],
+        ["","","","","","","",""],
+        ["","","","","","","",""]
         ],
       state: "new",
       mines: 9
@@ -29,50 +29,104 @@ class App extends Component {
       .get("https://minesweeper-api.herokuapp.com/games")
       .then(response=>{
         let newState = response.data
-        this.setState = (newState)
+        this.setState(newState)
       })
-    
+  }
 
   render() {
-    let rows=this.state
-
-
-
+    let rows=this.state.board
     return (
       <div className="App">
         <head><link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet"/> </head>
           <h1>MINESWEEPER</h1>
-          <main >
+          <main > 
             <table className="playarea">
-              <tr>
-                <td colSpan={this.state.board[0].length}>
-                
-                {/* change this to a new game option after completion */}
-                {/* <button onclick={this.loadSampleGame}>Load Sample Game</button>
-                <p> you are playing game # {this.state.id}</p> */}
-                </td>
+              <tr> 
+                {/* this tells the top row how many columns wide it should be */}
+                <td colSpan={this.state.board.length}>
+
+                <button onclick={this.loadSampleGame}>Load Sample Game</button>
+                <p>you are playing game # {this.state.id} </p>  </td>
+              </tr> 
+              <tr> 
+                <td>{this.state.board[0][0]}</td>
+                <td>{this.state.board[0][1]}</td>
+                <td>{this.state.board[0][2]}</td>
+                <td>{this.state.board[0][3]}</td>
+                <td>{this.state.board[0][4]}</td>
+                <td>{this.state.board[0][5]}</td>
+                <td>{this.state.board[0][6]}</td>
+                <td>{this.state.board[0][7]}</td>
               </tr>
-              <tr> {this.state.board[0]}              </tr>
-              <tr>
-                <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+              <tr> 
+                <td>{this.state.board[1][0]}</td>
+                <td>{this.state.board[1][1]}</td>
+                <td>{this.state.board[1][2]}</td>
+                <td>{this.state.board[1][3]}</td>
+                <td>{this.state.board[1][4]}</td>
+                <td>{this.state.board[1][5]}</td>
+                <td>{this.state.board[1][6]}</td>
+                <td>{this.state.board[1][7]}</td>
               </tr>
-              <tr>
-                <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+              <tr> 
+                <td>{this.state.board[2][0]}</td>
+                <td>{this.state.board[2][1]}</td>
+                <td>{this.state.board[2][2]}</td>
+                <td>{this.state.board[2][3]}</td>
+                <td>{this.state.board[2][4]}</td>
+                <td>{this.state.board[2][5]}</td>
+                <td>{this.state.board[2][6]}</td>
+                <td>{this.state.board[2][7]}</td>
               </tr>
-              <tr>
-                <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+              <tr> 
+                <td>{this.state.board[3][0]}</td>
+                <td>{this.state.board[3][1]}</td>
+                <td>{this.state.board[3][2]}</td>
+                <td>{this.state.board[3][3]}</td>
+                <td>{this.state.board[3][4]}</td>
+                <td>{this.state.board[3][5]}</td>
+                <td>{this.state.board[3][6]}</td>
+                <td>{this.state.board[3][7]}</td>
               </tr>
-              <tr>
-                <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+              <tr> 
+                <td>{this.state.board[4][0]}</td>
+                <td>{this.state.board[4][1]}</td>
+                <td>{this.state.board[4][2]}</td>
+                <td>{this.state.board[4][3]}</td>
+                <td>{this.state.board[4][4]}</td>
+                <td>{this.state.board[4][5]}</td>
+                <td>{this.state.board[4][6]}</td>
+                <td>{this.state.board[4][7]}</td>
               </tr>
-              <tr>
-                <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+              <tr> 
+                <td>{this.state.board[5][0]}</td>
+                <td>{this.state.board[5][1]}</td>
+                <td>{this.state.board[5][2]}</td>
+                <td>{this.state.board[5][3]}</td>
+                <td>{this.state.board[5][4]}</td>
+                <td>{this.state.board[5][5]}</td>
+                <td>{this.state.board[5][6]}</td>
+                <td>{this.state.board[5][7]}</td>
               </tr>
-              <tr>
-                <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+              <tr> 
+                <td>{this.state.board[6][0]}</td>
+                <td>{this.state.board[6][1]}</td>
+                <td>{this.state.board[6][2]}</td>
+                <td>{this.state.board[6][3]}</td>
+                <td>{this.state.board[6][4]}</td>
+                <td>{this.state.board[6][5]}</td>
+                <td>{this.state.board[6][6]}</td>
+                <td>{this.state.board[6][7]}</td>
               </tr>
-              <tr>
-                <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+              <tr> 
+                <td>{this.state.board[7][0]}</td>
+                <td>{this.state.board[7][1]}</td>
+                <td>{this.state.board[7][2]}</td>
+                <td>{this.state.board[7][3]}</td>
+                <td>{this.state.board[7][4]}</td>
+                <td>{this.state.board[7][5]}</td>
+                <td>{this.state.board[7][6]}</td>
+                <td>{this.state.board[7][7]}</td>
               </tr>
             </table>
           </main>
---
+++ src/Board.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+class Board   extends Component {
+
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
+export default Board
