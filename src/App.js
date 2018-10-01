d0d2a4587491009b7934c6bff5af6c913673a570
matt <cookem529@gmail.com>
2018-10-01 16:16:27 -0400

    difficulty works! Thanks, Gavin! Working on different classes now

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Row from './Row'
 import axios from "axios";
 
+
 class App extends Component {
   constructor(props){
     super(props);
@@ -26,100 +27,69 @@ class App extends Component {
 
   newGame = (event) =>{
     axios
-      .post("https://minesweeper-api.herokuapp.com/games", {difficulty:0})
+      .post("https://minesweeper-api.herokuapp.com/games", {difficulty: event.target.dataset['value']})
       .then(response=>{
         this.setState(response.data)
       })
   }    
   check = (row, column)=>{
-    console.log(`I clicked!!!! on ${row} and ${column}`)
     axios
       .post(`https://minesweeper-api.herokuapp.com/games/${this.state.id}/check`, {row: row, col: column})
       .then(response=>{
         this.setState(response.data)
       })
   }
-
   flag=(row, column)=>{
-    console.log(`I clicked!!!! on ${row} and ${column}`)
     axios
       .post(`https://minesweeper-api.herokuapp.com/games/${this.state.id}/flag`, {row: row, col: column})
       .then(response=>{
         this.setState(response.data)
       })
   }
-
   render() {
-    // let board=this.state.board[0].map(Row=>{
-    //   return Row
-    // })
-
-
     return (
       <div className="App">
         <head><link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet"/></head>
           <h1>MINESWEEPER</h1>
-          <table className="playarea">
+          <table>
             <tbody>
               <tr>
                 {/* this tells the top row how many columns wide it should be  */}
                 <td colSpan={this.state.board.length}>
-                <button onClick={this.newGame}>Want to play?</button>
-                <p>you are playing game # {this.state.id}<br></br>
-                There are {this.state.mines} mines left </p></td>
+                <div class="dropdown">
+                  <button class="dropbtn">Difficulty</button>
+                  <div class="dropdown-content">
+                    <a onClick={this.newGame} data-value="0" title="If you're reading this you probably need a harder difficulty">easy(8x8)</a>
+                    <a onClick={this.newGame} data-value="1" title="Good luck, Broseidon!">Moderate(16x16)</a>
+                    <a onClick={this.newGame} data-value="2" title="Are you this much of a masochist in every part of your life?">Difficult(24x24)</a>
+                  </div>
+                </div>
+
+
+
+
+
+
+
+
+
+                <p>you are playing game # {this.state.id}</p>
+                There are {this.state.mines} mines left</td>
               </tr> 
-              
-              <tr>
-                {
-                  this.state.board[0].map((square, index)=>{
-                    return <Row square={square} row={0} column={index} check={this.check} flag={this.flag}/>
-                  })
-                }
-              </tr><tr>
-                {
-                  this.state.board[1].map((square, index)=>{
-                    return <Row square={square} row={1} column={index} check={this.check} flag={this.flag}/>
-                  })
-                }
-              </tr><tr>
-                {
-                  this.state.board[2].map((square, index)=>{
-                    return <Row square={square} row={2} column={index} check={this.check} flag={this.flag}/>
-                  })
-                }
-              </tr><tr>
-                {
-                  this.state.board[3].map((square, index)=>{
-                    return <Row square={square} row={3} column={index} check={this.check} flag={this.flag}/>
-                  })
-                }
-              </tr><tr>
-                {
-                  this.state.board[4].map((square, index)=>{
-                    return <Row square={square} row={4} column={index} check={this.check} flag={this.flag}/>
-                  })
-                }
-              </tr><tr>
-                {
-                  this.state.board[5].map((square, index)=>{
-                    return <Row square={square} row={5} column={index} check={this.check} flag={this.flag}/>
-                  })
-                }
-              </tr><tr>
-                {
-                  this.state.board[6].map((square, index)=>{
-                    return <Row square={square} row={6} column={index} check={this.check} flag={this.flag}/>
-                  })
-                }
-              </tr><tr>
-                {
-                  this.state.board[7].map((square, index)=>{
-                    return <Row square={square} row={7} column={index} check={this.check} flag={this.flag}/>
-                  })
-                }
-              </tr>
-              
-              </tbody>
+              {
+                this.state.board.map((row, rowIndex) =>{
+                  return(
+                    <tr>
+                      {
+                      row.map((square, index)=>{
+                        return <Row square={square} row={rowIndex} column={index} check={this.check} flag={this.flag}/>
+                      }) 
+                      }
+                    </tr>
+                  )
+                })
+              }
+            </tbody>
           </table> 
       </div>
     );
--- src/Row.js
+++ src/Row.js
@@ -3,18 +3,45 @@ import React, { Component } from 'react';
 class Row   extends Component {
     handleChoice = event => {
         this.props.check(this.props.row, this.props.column)
-        // since this worked as a console log of ' I clicked' I can use this to act as a fetch for the API for this specifically targeted cell
+        // since this worked as a console log of ' I clicked' I can use this to act as a fetch for the API for this specifically targeted cell but HOW????
     }
     flag= event => {
         this.props.flag(this.props.row, this.props.column)
         event.preventDefault()
-        // since this worked as a console log of ' I clicked' I can use this to act as a fetch for the API for this specifically targeted cell
 }
+
     render() {
+        let output=this.props.square
+        if(output === '*'){
+            output='💩'
+        }
+        if(output=== '_'){
+            output=' '
+        }
         return(
-            <td onClick={this.handleChoice} onContextMenu={this.flag}
-            >{this.props.square}</td>
+            <td onClick={this.handleChoice} onContextMenu={this.flag}>
+                {output}
+            </td>
         )
     }
 }
 export default Row
+
+
+// CLASS_NAMES = {
+//     '*': 'cell-bomb',
+//     F: 'cell-flag',
+//     '@': 'cell-flag',
+//     ' ': 'cell-covered'
+//   }
+
+//   VALUES = { '*': '💣', F: '⚑', '@': '⚑', _: ' ' }
+
+//   render() {
+//     return (
+//       <td
+//         className={this.CLASS_NAMES[this.props.value] || 'cell-exposed'}
+//         onContextMenu={this.handleFlag}
+//         onClick={this.handleCheck}
+//       >
+//         {this.VALUES[this.props.value] || this.props.value}
