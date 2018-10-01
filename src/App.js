a9d6287b7d9c694fb1469baecda33e28c91d8876
matt <cookem529@gmail.com>
2018-10-01 10:37:39 -0400

    Thank you, Gavin

--- src/App.js
+++ src/App.js
@@ -24,14 +24,30 @@ class App extends Component {
     };
   }
 
-  loadSampleGame = (event) =>{
+  newGame = (event) =>{
     axios
-      .get("https://minesweeper-api.herokuapp.com/games", {difficulty:0})
-      .post(`https://minesweeper-api.herokuapp.com/games/${this.state.id}`)
+      .post("https://minesweeper-api.herokuapp.com/games", {difficulty:0})
       .then(response=>{
         this.setState(response.data)
       })
   }    
+  check = (row, column)=>{
+    console.log(`I clicked!!!! on ${row} and ${column}`)
+    axios
+      .post(`https://minesweeper-api.herokuapp.com/games/${this.state.id}/check`, {row: row, col: column})
+      .then(response=>{
+        this.setState(response.data)
+      })
+  }
+
+  flag=(row, column)=>{
+    console.log(`I clicked!!!! on ${row} and ${column}`)
+    axios
+      .post(`https://minesweeper-api.herokuapp.com/games/${this.state.id}/flag`, {row: row, col: column})
+      .then(response=>{
+        this.setState(response.data)
+      })
+  }
 
   render() {
     // let board=this.state.board[0].map(Row=>{
@@ -48,85 +64,61 @@ class App extends Component {
               <tr>
                 {/* this tells the top row how many columns wide it should be  */}
                 <td colSpan={this.state.board.length}>
-                <button onClick={this.loadSampleGame}>Load Sample Game</button>
-                <p>you are playing game # {this.state.id} </p>  </td>
+                <button onClick={this.newGame}>Want to play?</button>
+                <p>you are playing game # {this.state.id} </p> 
+                <p>There are {this.state.mines} mines left</p> </td>
               </tr> 
-                {/* {board} */}
-
+              
               <tr>
-              <Row square={this.state.board[0][0]}/>
-              <Row square={this.state.board[0][1]}/>
-              <Row square={this.state.board[0][2]}/>
-              <Row square={this.state.board[0][3]}/>
-              <Row square={this.state.board[0][4]}/>
-              <Row square={this.state.board[0][5]}/>
-              <Row square={this.state.board[0][6]}/>
-              <Row square={this.state.board[0][7]}/>
+                {
+                  this.state.board[0].map((square, index)=>{
+                    return <Row square={square} row={0} column={index} check={this.check} flag={this.flag}/>
+                  })
+                }
+              </tr><tr>
+                {
+                  this.state.board[1].map((square, index)=>{
+                    return <Row square={square} row={1} column={index} check={this.check} flag={this.flag}/>
+                  })
+                }
+              </tr><tr>
+                {
+                  this.state.board[2].map((square, index)=>{
+                    return <Row square={square} row={2} column={index} check={this.check} flag={this.flag}/>
+                  })
+                }
+              </tr><tr>
+                {
+                  this.state.board[3].map((square, index)=>{
+                    return <Row square={square} row={3} column={index} check={this.check} flag={this.flag}/>
+                  })
+                }
+              </tr><tr>
+                {
+                  this.state.board[4].map((square, index)=>{
+                    return <Row square={square} row={4} column={index} check={this.check} flag={this.flag}/>
+                  })
+                }
+              </tr><tr>
+                {
+                  this.state.board[5].map((square, index)=>{
+                    return <Row square={square} row={5} column={index} check={this.check} flag={this.flag}/>
+                  })
+                }
+              </tr><tr>
+                {
+                  this.state.board[6].map((square, index)=>{
+                    return <Row square={square} row={6} column={index} check={this.check} flag={this.flag}/>
+                  })
+                }
+              </tr><tr>
+                {
+                  this.state.board[7].map((square, index)=>{
+                    return <Row square={square} row={7} column={index} check={this.check} flag={this.flag}/>
+                  })
+                }
               </tr>
-              <tr>
-              <Row square={this.state.board[1][0]}/>
-              <Row square={this.state.board[1][1]}/>
-              <Row square={this.state.board[1][2]}/>
-              <Row square={this.state.board[1][3]}/>
-              <Row square={this.state.board[1][4]}/>
-              <Row square={this.state.board[1][5]}/>
-              <Row square={this.state.board[1][6]}/>
-              <Row square={this.state.board[1][7]}/>
-              </tr> <tr>
-              <Row square={this.state.board[2][0]}/>
-              <Row square={this.state.board[2][1]}/>
-              <Row square={this.state.board[2][2]}/>
-              <Row square={this.state.board[2][3]}/>
-              <Row square={this.state.board[2][4]}/>
-              <Row square={this.state.board[2][5]}/>
-              <Row square={this.state.board[2][6]}/>
-              <Row square={this.state.board[2][7]}/>
-              </tr> <tr>
-              <Row square={this.state.board[3][0]}/>
-              <Row square={this.state.board[3][1]}/>
-              <Row square={this.state.board[3][2]}/>
-              <Row square={this.state.board[3][3]}/>
-              <Row square={this.state.board[3][4]}/>
-              <Row square={this.state.board[3][5]}/>
-              <Row square={this.state.board[3][6]}/>
-              <Row square={this.state.board[3][7]}/>
-              </tr> <tr>
-              <Row square={this.state.board[4][0]}/>
-              <Row square={this.state.board[4][1]}/>
-              <Row square={this.state.board[4][2]}/>
-              <Row square={this.state.board[4][3]}/>
-              <Row square={this.state.board[4][4]}/>
-              <Row square={this.state.board[4][5]}/>
-              <Row square={this.state.board[4][6]}/>
-              <Row square={this.state.board[4][7]}/>
-              </tr> <tr>
-              <Row square={this.state.board[5][0]}/>
-              <Row square={this.state.board[5][1]}/>
-              <Row square={this.state.board[5][2]}/>
-              <Row square={this.state.board[5][3]}/>
-              <Row square={this.state.board[5][4]}/>
-              <Row square={this.state.board[5][5]}/>
-              <Row square={this.state.board[5][6]}/>
-              <Row square={this.state.board[5][7]}/>
-              </tr> <tr>
-              <Row square={this.state.board[6][0]}/>
-              <Row square={this.state.board[6][1]}/>
-              <Row square={this.state.board[6][2]}/>
-              <Row square={this.state.board[6][3]}/>
-              <Row square={this.state.board[6][4]}/>
-              <Row square={this.state.board[6][5]}/>
-              <Row square={this.state.board[6][6]}/>
-              <Row square={this.state.board[6][7]}/>
-              </tr> <tr>
-              <Row square={this.state.board[7][0]}/>
-              <Row square={this.state.board[7][1]}/>
-              <Row square={this.state.board[7][2]}/>
-              <Row square={this.state.board[7][3]}/>
-              <Row square={this.state.board[7][4]}/>
-              <Row square={this.state.board[7][5]}/>
-              <Row square={this.state.board[7][6]}/>
-              <Row square={this.state.board[7][7]}/>
-              </tr>
+              
               </tbody>
           </table> 
       </div>
--- src/Row.js
+++ src/Row.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 
 class Row   extends Component {
-handleChoice = event => {
-    console.log("I clicked!!!!", this.props)
-// since this worked as a console log of ' I clicked' I can use this to act as a fetch for the API for this specifically targeted cell
+    handleChoice = event => {
+        this.props.check(this.props.row, this.props.column)
+        // since this worked as a console log of ' I clicked' I can use this to act as a fetch for the API for this specifically targeted cell
+    }
+    flag= event => {
+        this.props.flag(this.props.row, this.props.column)
+        event.preventDefault()
+        // since this worked as a console log of ' I clicked' I can use this to act as a fetch for the API for this specifically targeted cell
 }
-
     render() {
         return(
-            <td onClick={this.handleChoice} class="square covered unsolved" >  
-                {this.props.square}
-            </td>
+            <td onClick={this.handleChoice} onContextMenu={this.flag}
+            >{this.props.square}</td>
         )
     }
 }
